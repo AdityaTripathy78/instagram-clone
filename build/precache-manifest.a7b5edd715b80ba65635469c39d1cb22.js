7ff24a40351bf84fd787e6d1efcfa8544873fa66
aditya <adityalipu78@gmail.com>
2021-05-05 23:51:25 +0530

    initial commit

---
+++ build/precache-manifest.a7b5edd715b80ba65635469c39d1cb22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2598ef8abc70f382fde80675d9a6ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "220339233a0a214fb46b",
+    "url": "/static/css/main.52dddafe.chunk.css"
+  },
+  {
+    "revision": "ba6768106bf1eb9df2dd",
+    "url": "/static/js/2.fdd3e8d4.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.fdd3e8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220339233a0a214fb46b",
+    "url": "/static/js/main.e0767ca1.chunk.js"
+  },
+  {
+    "revision": "122bd53447d1f399042f",
+    "url": "/static/js/runtime-main.f7b531b7.js"
+  }
+]);
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBucH7Kzm8Ds5CbuUs8uQJlzTPh3eZcTzA",
-  authDomain: "instagram-clone-23884.firebaseapp.com",
-  databaseURL: "https://instagram-clone-23884.firebaseio.com",
-  projectId: "instagram-clone-23884",
-  storageBucket: "instagram-clone-23884.appspot.com",
-  messagingSenderId: "671034896143",
-  appId: "1:671034896143:web:3aceafdf2319c9f1fc587a",
+  apiKey: "AIzaSyDP5CLkwhdC5axr9x78vDMVhL3lErw8ZKo",
+    authDomain: "instagram-clone-846f7.firebaseapp.com",
+    databaseURL: "https://instagram-clone-846f7.web.app",
+    projectId: "instagram-clone-846f7",
+    storageBucket: "instagram-clone-846f7.appspot.com",
+    messagingSenderId: "481600320667",
+    appId: "1:481600320667:web:cc54fd208f264dec4a823e",
 });
 
 const db = firebaseApp.firestore();
